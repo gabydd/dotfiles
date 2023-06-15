215d7eceef220ba9d37c4da9f379412c03da5d57
gabydd <gabydinnerdavid@gmail.com>
2023-06-15 01:09:30 -0400

    start nixos

---
+++ configuration.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./home.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "gabydd";
+  networking.networkmanager.enable = true;
+  programs.nm-applet.enable = true;
+
+  time.timeZone = "America/Toronto";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
+
+  users.users.gaby = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+  };
+
+  fonts = {
+    fonts = with pkgs; [
+      iosevka
+      roboto
+      font-awesome
+      noto-fonts-emoji
+    ];
+    fontconfig.defaultFonts = {
+      monospace = ["Iosevka"];
+    };
+  };
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+  services.openssh.enable = true;
+  services.printing.enable = true;
+  services.dbus.enable = true;
+  security.polkit.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-wlr xdg-desktop-portal-gtk ];
+  };
+  system.stateVersion = "23.05";
+}
+
---
+++ home.nix
@@ -0,0 +1,141 @@
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  home-manager.users.gaby = {
+    home.stateVersion = "23.05";
+    home.packages = with pkgs; [
+      helix
+      wget
+      swaylock
+      wl-clipboard
+      grim
+      slurp
+      wf-recorder
+      wofi
+      imv
+      chromium
+      ripgrep
+      skim
+      xdg-utils
+      asciinema
+      fish
+      nil
+    ];
+
+
+    wayland.windowManager.sway = {
+      enable = true; 
+      config = {
+        modifier = "Mod4";
+        terminal = "kitty"; 
+        bars = [];
+        window = {
+          titlebar = false;
+        };
+      };
+      extraConfig = ''
+        default_border none
+        bar swaybar_command waybar
+      '';
+    };
+
+    programs.waybar = {
+      enable = true;
+      style = pkgs.lib.readFile ./waybar/style.css;
+      settings = [
+        {
+          layer = "bottom";
+          modules-left = ["sway/workspaces"];
+          modules-center = ["clock"];
+          modules-right = ["network" "cpu" "memory" "battery" "pulseaudio" "tray"];
+          tray = {
+            spacing = 10;
+          };
+          clock = {
+            format = "{:%I:%M %p}";
+            tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+            format-alt= "{:%m/%d/%y}";
+          };
+          cpu = {
+            format = " {usage}%";
+            tooltip = false;
+          };
+          memory = {
+            "format" = " {}%";
+          };
+          "temperature" = {
+            "critical-threshold" = 80;
+            "format" = "{temperatureC}°C {icon}";
+            "format-icons" = [
+              ""
+              ""
+              ""
+            ];
+          };
+          
+          "battery" = {
+            "states" = {
+              "warning" = 30;
+              "critical" = 15;
+            };
+            "format" = "{icon} {capacity}%";
+            "format-charging" = " {capacity}%";
+            "format-plugged" = " {capacity}%";
+            "format-alt" = "{time} {icon}";
+            "format-icons" = [
+              ""
+              ""
+              ""
+              ""
+              ""
+            ];
+          };
+          "network" = {
+            "format-wifi" = " {essid} ({signalStrength}%)";
+            "format-ethernet" = "{ipaddr}/{cidr}";
+            "tooltip-format" = " {ifname} via {gwaddr}";
+            "format-linked" = " {ifname} (No IP)";
+            "format-disconnected" = "⚠ Disconnected";
+            "format-alt" = "{ifname} = {ipaddr}/{cidr}";
+          };
+          "pulseaudio" = {
+            "format" = "{icon} {volume}% {format_source}";
+            "format-bluetooth" = "{icon} {volume}%  {format_source}";
+            "format-bluetooth-muted" = "  {icon}  {format_source}";
+            "format-muted" = "  {format_source}";
+            "format-source" = " {volume}%";
+            "format-source-muted" = " ";
+            "format-icons" = {
+              "headphone" = "";
+              "hands-free" = "";
+              "headset" = "";
+              "phone" = "";
+              "portable" = "";
+              "car" = "";
+              "default" = [
+                ""
+                ""
+                ""
+              ];
+            };
+            "on-click" = "pavucontrol";
+          };
+        }
+      ];
+    };
+    programs.firefox.enable = true;
+    programs.git = {
+      enable = true;
+      userName = "gabydd";
+      userEmail = "gabydinnerdavid@gmail.com";
+    };
+    programs.fish.enable = true;
+    programs.kitty.enable = true;
+  };
+}
